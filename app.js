f042b5bfc0943ab381a659bf4c025d5c593557ad
absent007 <127410273+absent007@users.noreply.github.com>
2023-03-13 17:22:44 +0600

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const shortenForm = document.querySelector("#shorten-form");
-const urlInput = document.querySelector("#url-input");
-const resultContainer = document.querySelector(".result-container");
-const shortUrl = document.querySelector("#short-url");
-const copyButton = document.querySelector("#copy-button
